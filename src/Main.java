c68cc90fdc1423e5b115935cff81e4d5c335c6bc
Antonmak112 <pyanton112@gmail.com>
2023-07-22 22:14:33 +0500

    inital

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,13 @@ public class Main {
 
         UpdateData updateData1 = new UpdateData(UpdateType.ACCOUNT,"2");//Получили обновление данных по Аккаунту 2 ордера..
         UpdateData updateData2 = new UpdateData(UpdateType.WALLET,"263 -32.5");//Получили обновление данных по Кошельку 263 бакса и -32.5 изменение..
+
         System.out.println(updateData1.getData());//тут мы не можем обратиться к методам класса AccountUpdate хотя это его данные
         System.out.println(updateData2.getData());//тут мы не можем обратиться к методам класса WalletUpdate хотя это его данные
+
+        //Ошибка
+        System.out.println(updateData1.getData().getOrders());
+        // getOrders() не работает как сделать что бы работал??
 
     }
 }
